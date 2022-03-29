629a506f735bd0603399c26b3b7d6bfe343fc8bb
Clément Wds <clementwalshdeserrant@outlook.com>
2022-03-29 15:11:39 +0200

    MAJ Routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/login', 'App\Http\Controllers\AuthController@login');
+Route::get('/logout', 'App\Http\Controllers\AuthController@logout');
+
+Route::post('/signin', 'App\Http\Controllers\UserController@createUser');
+Route::post('/signin', 'App\Http\Controllers\UserController@createUser');
+Route::post('/edit_profile', 'App\Http\Controllers\UserController@editUser');
+Route::post('/change_password', 'App\Http\Controllers\UserController@changePassword');
+Route::get('/delete_account', 'App\Http\Controllers\UserController@deleteUser');
