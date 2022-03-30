a9cb1ca37113da74ba25c857213e9fa2cb0aa4cf
Clément Wds <clementwalshdeserrant@outlook.com>
2022-03-30 16:00:06 +0200

    delete welcome view and route

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 //AUTHENTICATION
 Route::post('/login', 'App\Http\Controllers\AuthController@login');
 Route::get('/logout', 'App\Http\Controllers\AuthController@logout');
