e71529d74497c27d985f2dc57540cb674e684bc8
Clément Wds <clementwalshdeserrant@outlook.com>
2022-05-23 15:54:34 +0200

    maj authController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -29,7 +29,7 @@ class AuthController extends Controller
     }
 
     public function logout(){
-        auth()->lougout();
+        auth()->logout();
         //Vous êtes déconnectés
         return redirect('/');
     }
